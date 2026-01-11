8c344b97f6828c48779efbace17be9cd36c6267f
Raginaakhter <raginaakhter052@gmail.com>
2026-01-12 00:34:19 +0600

    updated

---
+++ src/components/AdminDashboard/Dashboard/Demo.tsx
@@ -0,0 +1,12 @@
+
+
+const Demo = () => {
+    return (
+        <div>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore nesciunt dignissimos quos cupiditate, enim accusantium reprehenderit dolor numquam aliquid 
+            culpa neque saepe, ducimus vero? Maiores facilis impedit ducimus sit repellendus.
+        </div>
+    );
+};
+
+export default Demo;
--- src/redux/types/Demo.tsx
+++ src/redux/types/Demo.tsx
@@ -4,7 +4,7 @@ const Demo = () => {
     return (
         <div>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error eaque dolor maxime tempora. Sunt, iure soluta, porro quos obcaecati vero beatae,
-             laboriosam architecto blanditiis molestias earum incidunt eos laborum dolorem!
+             laboriosam architecto blanditiis molestias 
         </div>
     );
 };
