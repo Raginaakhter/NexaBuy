9dfca0fc058e150c069e3eacd9cf57705c877cfb
Raginaakhter <raginaakhter052@gmail.com>
2026-01-18 21:56:28 +0600

    added lorem text

--- src/components/AdminDashboard/Dashboard/Demo.tsx
+++ src/components/AdminDashboard/Dashboard/Demo.tsx
@@ -4,7 +4,8 @@ const Demo = () => {
     return (
         <div>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore nesciunt dignissimos quos cupiditate, enim accusantium reprehenderit dolor numquam aliquid 
-         
+         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt debitis laborum veniam eius, est ratione recusandae fugit hic! Labore sequi iure nulla totam voluptatem
+          laudantium pariatur autem odit recusandae obcaecati!
         </div>
     );
 };
