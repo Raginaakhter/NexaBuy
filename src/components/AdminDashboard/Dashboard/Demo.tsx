e3ef3503532b40b8389df556212a408b7e7f80d6
Raginaakhter <raginaakhter052@gmail.com>
2026-01-18 22:03:35 +0600

    addedtext demo lorem

--- src/components/AdminDashboard/Dashboard/Demo.tsx
+++ src/components/AdminDashboard/Dashboard/Demo.tsx
@@ -5,8 +5,7 @@ const Demo = () => {
         <div>
             <h1 className="text-amber-600">this is demo page</h1>
           <button>click here</button>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi reprehenderit odit atque perspiciatis natus placeat iusto inventore cupiditate tempora praesentium tempore quod, adipisci nihil doloribus neque laboriosam illum officiis facilis!
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore nesciunt dignissimos quos cupiditate, enim accusantium reprehenderit dolor numquam aliquid  estiae nisi. Ea distinctio illo fugiat ut excepturi esse molestiae officia, reiciendis illum.
+          Lorem ipsum dolor sit amet consectetur,fgfgfg m, ipsum dolor sit amet consectetur adipisicing elit. Labore nesciunt dignissimos quos cupiditate, enim accusantium reprehenderit dolor numquam aliquid  estiae nisi. Ea distinctio illo fugiat ut excepturi esse molestiae officia, reiciendis illum.
         </div>
     );
 };
