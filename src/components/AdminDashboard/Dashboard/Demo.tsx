33ef43b1f3a41397aca2814607c1e2bd026e0d38
Raginaakhter <raginaakhter052@gmail.com>
2026-01-18 22:00:36 +0600

    updated code

--- src/components/AdminDashboard/Dashboard/Demo.tsx
+++ src/components/AdminDashboard/Dashboard/Demo.tsx
@@ -3,7 +3,7 @@
 const Demo = () => {
     return (
         <div>
-            <h1>this is demo page</h1>
+            <h1 className="text-amber-600">this is demo page</h1>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore nesciunt dignissimos quos cupiditate, enim accusantium reprehenderit dolor numquam aliquid  estiae nisi. Ea distinctio illo fugiat ut excepturi esse molestiae officia, reiciendis illum.
         </div>
     );
