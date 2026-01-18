e61acb1a695d0405609ffbaa0e7057a6480ad5dc
Raginaakhter <raginaakhter052@gmail.com>
2026-01-18 22:10:00 +0600

    added

--- src/components/AdminDashboard/Dashboard/Demo.tsx
+++ src/components/AdminDashboard/Dashboard/Demo.tsx
@@ -6,7 +6,7 @@ const Demo = () => {
             <h1 className="text-amber-600">this is demo page</h1>
           <button>click here</button>
           <button>Read more into text</button>
-           consectetur,fgfgfg m, ipsum dolor sit Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus, consequatur quod! Reiciendis voluptatum dolore praesentium possimus ratione? Repudiandae illo amet non eni
+           consectetur,fgfgfg m, ipsum dolor sit Lorem ipsum hklhklhklhklhklhklhkl dolor, sit amet consectetur adipisicing elit. Repellendus, consequatur quod! Reiciendis voluptatum dolore praesentium possimus ratione? Repudiandae illo amet non eni
            m quidem animi excepturi, accusamus quaerat optio sequi libero! amet consectetur adipisicing elit. Labore nesciunt dignissimos quos cupiditate, enim accusantium reprehenderit dolor numquam aliquid  estiae nisi. Ea distinctio illo fugiat ut excepturi esse molestiae officia, reiciendis illum.
         </div>
     );
