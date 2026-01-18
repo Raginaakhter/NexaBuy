21ee7baa5e5fcc521cbed387f73e091363b1046b
Raginaakhter <raginaakhter052@gmail.com>
2026-01-18 22:05:05 +0600

    added button text

--- src/components/AdminDashboard/Dashboard/Demo.tsx
+++ src/components/AdminDashboard/Dashboard/Demo.tsx
@@ -5,7 +5,7 @@ const Demo = () => {
         <div>
             <h1 className="text-amber-600">this is demo page</h1>
           <button>click here</button>
-          <button></button>
+          <button>Read more</button>
           Lorem ipsum dolor sit amet consectetur,fgfgfg m, ipsum dolor sit amet consectetur adipisicing elit. Labore nesciunt dignissimos quos cupiditate, enim accusantium reprehenderit dolor numquam aliquid  estiae nisi. Ea distinctio illo fugiat ut excepturi esse molestiae officia, reiciendis illum.
         </div>
     );
