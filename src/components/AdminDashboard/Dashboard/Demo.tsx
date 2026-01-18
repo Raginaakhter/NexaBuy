6f075ce8a65b4feaa8a1db6884220d4e530359fe
Raginaakhter <raginaakhter052@gmail.com>
2026-01-18 22:01:08 +0600

    added button

--- src/components/AdminDashboard/Dashboard/Demo.tsx
+++ src/components/AdminDashboard/Dashboard/Demo.tsx
@@ -4,6 +4,7 @@ const Demo = () => {
     return (
         <div>
             <h1 className="text-amber-600">this is demo page</h1>
+          <button></button>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore nesciunt dignissimos quos cupiditate, enim accusantium reprehenderit dolor numquam aliquid  estiae nisi. Ea distinctio illo fugiat ut excepturi esse molestiae officia, reiciendis illum.
         </div>
     );
