29c3b76269e665182891cd770dc8db5cf23c5801
Raginaakhter <raginaakhter052@gmail.com>
2026-01-19 23:52:36 +0600

    added

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -4,7 +4,7 @@ const Cart = () => {
     return (
         <div>
             this is cart consectetur adipisicing elit. Aliquid illo accusantium hic laborum maxime natus exercitationem nobis enim voluptatibus magni? Quod 
-            eaque reiciendis vel, soluta obcaecati 
+           
         </div>
     );
 };
