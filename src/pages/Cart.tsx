3d8b44dda6d86940b79eca794fbb2490ba9c313f
Raginaakhter <raginaakhter052@gmail.com>
2026-01-19 23:47:29 +0600

    updated

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -3,7 +3,7 @@
 const Cart = () => {
     return (
         <div>
-            this is cart pages Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum eaque minima ipsa pariatur similique libero dignissimos veniam, culpa dolorem doloremque nobis ratione maxime sunt. Alias magni hic eos incidunt facilis!
+            this is cart pages Loremadipisicing elit. Nostrum eaque minima ipsa pariatur similique libero dignissimos veniam, culpa dolorem doloremque nobis ratione maxime sunt. Alias magni hic eos incidunt facilis!
         </div>
     );
 };
