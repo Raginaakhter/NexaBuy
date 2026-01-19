a66f2820d6e3d3d1fcd5c46b7848afe56b2f11b1
Raginaakhter <raginaakhter052@gmail.com>
2026-01-19 23:49:17 +0600

    added

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -3,7 +3,7 @@
 const Cart = () => {
     return (
         <div>
-            this is cart pages L minima iur similique libero dignissimos veniam, culpa dolorem doloremque nobis ratione maxime sunt. Alias magni hic eos incidunt facilis!
+            this is cart pages L minima iur culpa dolorem doloremque nobis ratione maxime sunt. Alias magni hic eos incidunt facilis!
         </div>
     );
 };
