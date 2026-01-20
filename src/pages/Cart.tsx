67af16ecf2c5c453c276f21d7642c2808f1650d5
Raginaakhter <raginaakhter052@gmail.com>
2026-01-20 23:28:37 +0600

    updated

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -3,7 +3,7 @@
 const Cart = () => {
     return (
         <div>
-            this is cart consectetur  sit amet consectetur adipisicing elit.jhhhhh Voluptatem quia a atq dignissimos veniam odit eveniet sit laborum atque cum nobis mollitia!
+            this is cart consectetur  sit amet consectetur jhhhhhhhhhhh adipisicing elit.jhhhhh Voluptatem quia a atq dignissimos veniam odit eveniet sit laborum atque cum nobis mollitia!
            
         </div>
     );
