60897a35a73cd5d7d6a0421d07d9430c1e1041ee
Raginaakhter <raginaakhter052@gmail.com>
2026-01-22 23:51:59 +0600

    added text

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -5,7 +5,7 @@ const Cart = () => {
         <div>
            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium odi
            o obcaecati vero atque ipsam dicta nam sit, praesentium accusamus voluptatem molestiae iusto delectus cum? Totam autem temporibus blanditiis ab atque.
-           <button></button>
+           <button>click here</button>
         </div>
     );
 };
