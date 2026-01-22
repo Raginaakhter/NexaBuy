6a179b0149cf3ea8d8b49809442ea9d04c50a6a2
Raginaakhter <raginaakhter052@gmail.com>
2026-01-22 23:55:27 +0600

    addedr

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -7,6 +7,7 @@ const Cart = () => {
            o obcaecati vero atque ipsam dicta nam sit, praesentium accusamus voluptatem molestiae iusto delectus cum? Totam autem temporibus blanditiis ab atque.
            <button className="text-amber-200" >click here</button>
            <h1>This is cart page</h1>
+           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi voluptas, commodi fugiat id dicta odio! Molestias deleniti temporibus accusamus deserunt, sequi ullam quas quo ratione quibusdam voluptates optio ipsum nulla.
         </div>
     );
 };
