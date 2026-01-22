ec2229d9a328cdc845b7add366f586eefccae600
Raginaakhter <raginaakhter052@gmail.com>
2026-01-22 23:54:16 +0600

    added colour

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -5,7 +5,7 @@ const Cart = () => {
         <div>
            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium odi
            o obcaecati vero atque ipsam dicta nam sit, praesentium accusamus voluptatem molestiae iusto delectus cum? Totam autem temporibus blanditiis ab atque.
-           <button>click here</button>
+           <button className="text-amber-200" >click here</button>
         </div>
     );
 };
