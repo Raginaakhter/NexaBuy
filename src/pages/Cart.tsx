29d1d357d052759125671502c071d869cad806bc
Raginaakhter <raginaakhter052@gmail.com>
2026-01-23 00:03:27 +0600

    added texts

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -3,7 +3,7 @@
 const Cart = () => {
     return (
         <div>
-           Loadipisicing elit. Accusantium odit,us voluptatem molestiae iusto delectus cum? Totam autem temporibus blanditiis ab atque.
+           sicing elit. Accusantium odit,us voluptatem molestiae iusto delectus cum? Totam autem temporibus blanditiis ab atque.
            <button className="text-amber-200" >click </button>
        
            Lorem ipsum dolor sit amet consectetur 
