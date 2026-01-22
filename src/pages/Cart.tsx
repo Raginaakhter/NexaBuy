bcf9b98a4c914c07438e986f21665600f5b53066
Raginaakhter <raginaakhter052@gmail.com>
2026-01-22 23:51:16 +0600

    added button

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -3,8 +3,9 @@
 const Cart = () => {
     return (
         <div>
-           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium odio obcaecati vero atque ipsam dicta nam sit, praesentium accusamus voluptatem molestiae iusto delectus cum? Totam autem temporibus blanditiis ab atque.
-           
+           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium odi
+           o obcaecati vero atque ipsam dicta nam sit, praesentium accusamus voluptatem molestiae iusto delectus cum? Totam autem temporibus blanditiis ab atque.
+           <button></button>
         </div>
     );
 };
