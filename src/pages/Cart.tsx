990c14c8f145a042e649a40f27d63aa5d5e0b47a
Raginaakhter <raginaakhter052@gmail.com>
2026-01-23 00:00:46 +0600

    added detelatehhhhhh

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -4,7 +4,7 @@ const Cart = () => {
     return (
         <div>
            Loadipisicing elit. Accusantium odi
-           o obcaecati vero atque ipsam dicta nam sit, praesentium accusamus voluptatem molestiae iusto delectus cum? Totam autem temporibus blanditiis ab atque.
+           o obcaecati vero atque ipsam dicta nam sit,us voluptatem molestiae iusto delectus cum? Totam autem temporibus blanditiis ab atque.
            <button className="text-amber-200" >click here</button>
        
            Lorem ipsum dolor sit amet consectetur 
