acfba8340b5fd40a69b27041cef33598ca9bf9b6
Raginaakhter <raginaakhter052@gmail.com>
2026-01-22 23:58:11 +0600

    added detelatehhhhhh

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -6,7 +6,7 @@ const Cart = () => {
            Loadipisicing elit. Accusantium odi
            o obcaecati vero atque ipsam dicta nam sit, praesentium accusamus voluptatem molestiae iusto delectus cum? Totam autem temporibus blanditiis ab atque.
            <button className="text-amber-200" >click here</button>
-        fguiuiuiuiuiuiuiuiuiuiuiuiui
+        fguiuiuiuiuiuiuiuiuiuiuiuiui jkkkkkkkkkkkkkkkk
            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi voluptas, commodi fugiat id dicta odio! Molestias deleniti temporibus accusamus deserunt,
             sequi ullam quas quo ratione quibusdam voluptates optio ipsum nulla.
         </div>
