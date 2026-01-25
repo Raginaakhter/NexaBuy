29d8390741b379ba4841ab91e83b9296d4f86730
Raginaakhter <raginaakhter052@gmail.com>
2026-01-25 23:59:12 +0600

    updated

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -3,7 +3,7 @@
 const Search = () => {
     return (
         <div>
-            This is search page 11 also aded Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, quibusdam perferendis iste dolor numquam nobis! Voluptas nesciunt numquam, architecto consequatur at voluptatum temporibus dignissimos impedi
+            This is search page 11 hjkkk also aded Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, quibusdam perferendis iste dolor numquam nobis! Voluptas nesciunt numquam, architecto consequatur at voluptatum temporibus dignissimos impedi
             t nobis nemo repudiandae, similique beatae?hhhhhhhhhhhhhgit
         </div>
     );
