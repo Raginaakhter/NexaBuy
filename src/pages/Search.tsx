fc8a3cd64201d9657652a3849bf8e1d76b653e82
Raginaakhter <raginaakhter052@gmail.com>
2026-01-26 23:58:15 +0600

    updated

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -5,8 +5,8 @@ const Search = () => {
         <div>
             This is search page 11 hjkkk also aded Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, quibusdam perferendis iste dolor numquam nobis! Voluptas nesciunt numquam, architecto consequatur at voluptatum temporibus dignissimos impedi
             t nobis nemo repudiandae,  Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere accusantium laborum facilis officia corrupti sapiente id nam mollitia iste doloribus, recusandae itaque laudantium veniam asperiores nesciunt e
-            arum voluptas et nulla.
-        </div>
+            arum voluptas et nulla. kth;;;;;;;;klegugugguekirwef
+        </div>  
     );
 };
 
