6d1c4e6a6ec58b67edbc19a0b2002556a400b8d9
Raginaakhter <raginaakhter052@gmail.com>
2026-03-13 23:58:34 +0600

    updated

--- src/redux/types/Demo.tsx
+++ src/redux/types/Demo.tsx
@@ -3,7 +3,7 @@
 const Demo = () => {
     return (
         <div>
-            Lorem, ipsum dolor  Error eaque dolor maxime tempora. Sunt, iure soluta, porro quos obcaecati vero beatae,
+            Lorem, ipsum  eaque dolor maxime tempora. Sunt, iure soluta, porro quos obcaecati vero beatae,
              
         </div>
     );
