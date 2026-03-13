f29a28075c19b4ebdc9d6b864234692cb14b0618
Raginaakhter <raginaakhter052@gmail.com>
2026-03-14 00:01:58 +0600

    updated

--- src/redux/types/Demo.tsx
+++ src/redux/types/Demo.tsx
@@ -3,7 +3,7 @@
 const Demo = () => {
     return (
         <div>
-            Lorem, ipsum . Sunt, iure soluta, porro quos obcaecati vero beatae,Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus totam est voluptates, distinctio  eligendi ipsum corrupti fuga soluta iste! Eius.
+            Lorem, ipsum . Sunt, iure soluta, porro quos obcaecati vero beatae,Lorem ipsum dolor sit amet, consectetur  eligendi ipsum corrupti fuga soluta iste! Eius.
              
         </div>
     );
