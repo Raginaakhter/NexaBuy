097f6f89c0533a191dfd051991cd507040bb2fd5
Raginaakhter <raginaakhter052@gmail.com>
2026-03-15 01:02:57 +0600

    updatedd

--- src/redux/types/Demo.tsx
+++ src/redux/types/Demo.tsx
@@ -5,7 +5,7 @@ const Demo = () => {
         <div>
         sol quos obcaecati vero beatae,Lorem ipsum dolor sit amet, consectetur  eligendi ipsum corrupti fuga soluta iste! Eius.
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium, sapiente quisquam ex praesentium ut quod laborum. In provident tenetur ut porro quibusdam 
-            nesciunt maxime cum, reiciendis inventore consectetur. Facere, tenetur? 
+         consectetur. Facere, tenetur? 
          Voluptatum, quia magni deleniti delectus ullam ex ut officia totam suscipit at odio minima, cum
             
         </div>
