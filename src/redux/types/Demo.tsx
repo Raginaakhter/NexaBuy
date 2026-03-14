effb67fedac40c5923656f810274be0672a8fa63
Raginaakhter <raginaakhter052@gmail.com>
2026-03-15 01:01:51 +0600

    updatedd

--- src/redux/types/Demo.tsx
+++ src/redux/types/Demo.tsx
@@ -7,7 +7,7 @@ const Demo = () => {
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium, sapiente quisquam ex praesentium ut quod laborum. In provident tenetur ut porro quibusdam 
             nesciunt maxime cum, reiciendis inventore consectetur. Facere, tenetur? 
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, quia magni deleniti delectus ullam ex ut officia totam suscipit at odio minima, cum
-            , exercitationem quibusdam maiores ad iure porro inventore.
+            
         </div>
     );
 };
