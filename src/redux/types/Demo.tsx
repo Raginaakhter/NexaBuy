7aee540fd55341f0c5b3c71d0827d56cf35400e0
Raginaakhter <raginaakhter052@gmail.com>
2026-04-09 16:29:18 +0600

    updatedd

--- src/redux/types/Demo.tsx
+++ src/redux/types/Demo.tsx
@@ -5,7 +5,7 @@ const Demo = () => {
         <div>
         sol quos obcaecati vero beatae,Lorem ipsum dolor sit amet, consectetur  eligendi ipsum corrupti fuga soluta iste! Eius.
           consectetur adipisicing elit. Facilis possimus dolore nostrum necessitatibus delectus perferendis expedita molestias soluta alias architecto praesentium, ea aspernatur voluptate omnis, modi, laboriosam voluptates iure libero?
-        molestias ipsam earum sed modi illum officiis blanditiis, velit reiciendis neque corporis odit, deleniti eveniet!
+         velit reiciendis neque corporis odit, deleniti eveniet!
         </div>
     );
 };
