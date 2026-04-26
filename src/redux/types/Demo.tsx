18199bd4bf8699675c395ddd6284c41a47bded4c
Raginaakhter <raginaakhter052@gmail.com>
2026-04-27 00:00:59 +0600

    updated demo

--- src/redux/types/Demo.tsx
+++ src/redux/types/Demo.tsx
@@ -3,7 +3,7 @@
 const Demo = () => {
     return (
         <div>
-        sol quos obcaecati vero beatae,Lorem ipsum dolor sit amet, consectetur  eligendi ipsum corrupti fuga soluta iste! Eius.
+        sol quos obcaecati vero beatae ipsum dolor sit amet, consectetur  eligendi ipsum corrupti fuga soluta iste! Eius.
           consectetur adipisicing elit. Facilis 
             architecto praesentium,  voluptate omnis, modi, laboriosam voluptates iure libero?
          velit 
